193374800fd202cfaf17fbcbbbeef0e0cb09f7b1
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-07-15 12:43:41 -0400

    creacion de AdministradoresRequestDTO de ms-adminisradores

---
+++ src/main/java/notaria/jenny/ms_administradores/dto/AdministradoresRequestDTO.java
@@ -0,0 +1,33 @@
+package notaria.jenny.ms_administradores.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import notaria.jenny.ms_administradores.model.Administradores;
+
+@Data
+public class AdministradoresRequestDTO {
+
+    @NotBlank
+    @Size(max = 200)
+    private String nombreCompleto;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    @Size(max = 20)
+    private String telefono;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank
+    @Size(max = 12)
+    private String rut;
+
+    @NotNull
+    private Administradores.Rol rol;
+}
