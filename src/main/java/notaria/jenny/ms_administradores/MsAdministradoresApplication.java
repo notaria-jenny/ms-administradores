222e36f5d84592282bed41ff3174de6979843ee3
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-07-15 12:31:31 -0400

    creacion de ms-administradores

---
+++ src/main/java/notaria/jenny/ms_administradores/MsAdministradoresApplication.java
@@ -0,0 +1,13 @@
+package notaria.jenny.ms_administradores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdministradoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdministradoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notaria/jenny/ms_administradores/MsAdministradoresApplicationTests.java
@@ -0,0 +1,13 @@
+package notaria.jenny.ms_administradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdministradoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
